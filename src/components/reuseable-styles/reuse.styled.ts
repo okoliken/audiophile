f59bcfb5cf3973eccba8fd1fa927e3d8419e3ad4
JEFF OKOLI <jahbuchimokoli3@gmail.com>
2023-12-13 22:51:38 +0100

    updates to ui

--- src/components/reuseable-styles/reuse.styled.ts
+++ src/components/reuseable-styles/reuse.styled.ts
@@ -16,7 +16,7 @@ export const Container = styled.div`
   }
   @media (${device.desktop}) {
     margin: 0 auto;
-    max-width: 2300px;
+    max-width: 1650px;
   }
 `;
 
@@ -204,5 +204,233 @@ export const HeroBanner = styled.div`
 
   @media (${device.desktop}) {
     background-position: center;
+  }
+`;
+
+
+export const SubSection = styled.div`
+  display: flex;
+  padding: 20px;
+  gap: 0px;
+  img {
+    border-radius: 10px;
+  }
+  .subsection__headset_xy_image-tablet {
+    display: none;
+  }
+  .subsection__content {
+    h2 {
+      color: #000;
+      font-size: 24px;
+      text-align: center;
+      margin-bottom: 12px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+    }
+
+    span {
+      color: #d87d4a;
+    }
+    p {
+      color: #000;
+      margin: 12px 0px;
+      text-align: center;
+      font-family: Manrope;
+      font-size: 15px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 25px;
+      opacity: 0.5;
+    }
+  }
+
+  .subsection__headset_xy_image-mobile {
+    margin: 30px 0px;
+    display: block;
+  }
+
+  .subsection__headset_xy_image-desktop {
+    display: none;
+  }
+  @media (${device.mobileS}) {
+    align-items: center;
+    justify-content: center;
+    flex-direction: column-reverse;
+    gap: 0px;
+    .subsection__content {
+      h2 {
+        font-size: 28px;
+        margin-bottom: 16px;
+        font-style: normal;
+        text-align: center;
+        font-weight: 700;
+        line-height: normal;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+      }
+      p {
+        color: #000;
+        margin: 12px 0px;
+        text-align: center;
+        font-family: Manrope;
+        font-size: 15px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 25px;
+        opacity: 0.5;
+      }
+    }
+    .subsection__headset_xy_image-mobile {
+      margin: 30px 0px;
+      display: block;
+    }
+
+    .subsection__headset_xy_image-desktop {
+      display: none;
+    }
+
+    .subsection__headset_xy_image-tablet {
+      display: none;
+    }
+  }
+  @media (${device.tablet}) {
+    align-items: center;
+    justify-content: center;
+    flex-direction: column-reverse;
+    min-height: 100vh;
+    gap: 0px;
+    .subsection__headset_xy_image-mobile {
+      display: none;
+    }
+
+    .subsection__content {
+      h2 {
+        font-size: 40px;
+        margin-bottom: 26px;
+        font-style: normal;
+        text-align: center;
+        font-weight: 700;
+        padding: 0 55px;
+        line-height: 44px; /* 110% */
+        letter-spacing: 1.429px;
+        text-transform: uppercase;
+      }
+      p {
+        color: #000;
+        margin: 12px 0px;
+        text-align: center;
+        font-family: Manrope;
+        font-size: 15px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 25px;
+        opacity: 0.5;
+      }
+    }
+
+    .subsection__headset_xy_image-desktop {
+      display: none;
+    }
+
+    .subsection__headset_xy_image-tablet {
+      display: block;
+      margin-bottom: 55px;
+    }
+  }
+  @media (${device.laptop}) {
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    min-height: 100vh;
+    gap: 0 84px;
+    .subsection__headset_xy_image-mobile {
+      display: none;
+    }
+
+    .subsection__content {
+      h2 {
+        font-size: 40px;
+        margin-bottom: 26px;
+        font-style: normal;
+        padding: 0px;
+        text-align: left;
+        font-weight: 700;
+        line-height: 44px;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+      }
+      p {
+        color: #000;
+        margin: 12px 0px;
+        text-align: justify;
+        font-family: Manrope;
+        font-size: 15px;
+        width: inherit;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 25px;
+        opacity: 0.5;
+      }
+    }
+
+    .subsection__headset_xy_image-desktop {
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      min-height: 600px;
+      object-fit: cover;
+    }
+    .subsection__headset_xy_image-tablet {
+      display: none;
+    }
+  }
+  @media (${device.desktop}) {
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    min-height: 100vh;
+    gap: 0 104px;
+    .subsection__headset_xy_image-mobile {
+      display: none;
+    }
+
+    .subsection__content {
+      h2 {
+        font-size: 50px;
+        margin-bottom: 26px;
+        font-style: normal;
+        padding: 0px;
+        text-align: left;
+        font-weight: 700;
+        line-height: 44px;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+      }
+      p {
+        color: #000;
+        margin: 12px 0px;
+        text-align: justify;
+        font-family: Manrope;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 35px;
+        opacity: 0.5;
+      }
+    }
+
+    .subsection__headset_xy_image-desktop {
+      display: block;
+      width: 100%;
+      max-width: 1200px;
+      min-height: 800px;
+      object-fit: cover;
+    }
+    .subsection__headset_xy_image-tablet {
+      display: none;
+    }
   }
 `;
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,12 +2,53 @@ import { Navbar } from "../components/navbar/Navbar";
 import { PageFooter } from "../components/footer/Footer";
 import { Outlet } from "react-router-dom";
 
+import HeadSet from "../assets/Bitmap-headset-mobile.png";
+import HeadSetDesktop from "../assets/Bitmap-headset.png";
+import HeadSetTablet from "../assets/Bitmap-headset-tablet.png";
+import {
+  Container,
+  SubSection,
+} from "../components/reuseable-styles/reuse.styled";
 
 export const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+
+      <Container>
+        <SubSection>
+          <div className="subsection__content">
+            <h2>
+              Bringing you the <span>best</span> audio gear
+            </h2>
+            <p>
+              Located at the heart of New York City, Audiophile is the premier
+              store for high end headphones, earphones, speakers, and audio
+              accessories. We have a large showroom and luxury demonstration
+              rooms available for you to browse and experience a wide range of
+              our products. Stop by our store to meet some of the fantastic
+              people who make Audiophile the best place to buy your portable
+              audio equipment.
+            </p>
+          </div>
+          <img
+            className="subsection__headset_xy_image-mobile"
+            src={HeadSet}
+            alt="headset"
+          />
+          <img
+            className="subsection__headset_xy_image-desktop"
+            src={HeadSetDesktop}
+            alt="headset"
+          />
+          <img
+            className="subsection__headset_xy_image-tablet"
+            src={HeadSetTablet}
+            alt="headset"
+          />
+        </SubSection>
+      </Container>
       <PageFooter />
     </>
   );
