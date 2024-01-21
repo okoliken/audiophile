b908711652b62b56f2188b164233c91becce8cb7
JEFF OKOLI <jahbuchimokoli3@gmail.com>
2024-01-21 19:05:33 +0100

    more updates to project

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,14 +1,5 @@
 import { supabase } from "../server/supabase";
 
-export const all_headsets = await supabase
-  .from("HEADSETS")
-  .select("image, product_name, description, sub_title, sub_title, slug")
-  .order("created_at");
-
-export const all_speakers = await supabase
-  .from("SPEAKERS")
-  .select("image, product_name, description, sub_title, sub_title, slug")
-  .order("created_at");
 
 export const getHeadsets = async () => {
  const data = await supabase
