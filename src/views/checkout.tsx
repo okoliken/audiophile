e2c03382e45aef94b49347bf78917ec4706d4cfa
okoliken <jahbuchimokoli3@gmail.com>
2024-02-26 23:53:55 +0100

    updates made to checkout page

--- src/views/checkout.tsx
+++ src/views/checkout.tsx
@@ -6,14 +6,46 @@ import {
   Grid,
 } from "../styles/reuseables.styled";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { Formik, Field, Form } from "formik";
+import { useEffect, useState, useReducer } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import { Button } from "../components/button/Button";
 
 import { Options } from "../utils/types";
 import { List } from "../components/product/list";
 import { FlexItem } from "../styles/reuseables.styled";
 import { PaymentOptionCard } from "../styles/styles.styled";
+
+type ReducerAction = {
+  type: string;
+  fieldName: string;
+  value: string;
+};
+
+type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
+interface FormState {
+  field1: string;
+  field2: string;
+  field3: string;
+  field4: string;
+  field5: string;
+  field6: string;
+  field7: string;
+  field8: string;
+  field9: string;
+}
+
+
+const initialState: FormState = {
+  field1: "",
+  field2: "",
+  field3: "",
+  field4: "",
+  field5: "",
+  field6: "",
+  field7: "",
+  field8: "",
+  field9: "",
+};
 
 export const CheckOut = () => {
   const navigate = useNavigate();
@@ -22,7 +54,54 @@ export const CheckOut = () => {
     value: "",
   });
 
-  
+  function formReducer(state: FormState, action: { type: string; fieldName: keyof FormState; value: string }): FormState {
+    switch (action.type) {
+      case "updateField":
+        return { ...state, [action.fieldName]: action.value };
+      default:
+        throw new Error();
+    }
+  }
+
+ 
+
+  const [state, dispatch] = useReducer(formReducer, initialState);
+
+  const updateField = (fieldName: keyof FormState, value: string) => {
+    dispatch({ type: "updateField", fieldName, value });
+  };
+
+  const validate = (values: FormState) => {
+
+    const errors:FormState  = {
+      field1: "",
+      field2: "",
+      field3: "",
+      field4: "",
+      field5: "",
+      field6: "",
+      field7: "",
+      field8: "",
+      field9: "",
+    };
+
+    if (!values.field2) {
+
+      errors.field2 = 'Required';
+
+    } else if (
+
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.field1)
+
+    ) {
+
+      errors.field2 = 'Invalid email address';
+
+    }
+
+    return errors;
+
+  }
 
   const selectPaymentOption = (val: Options) => {
     setPaymentOption(val);
@@ -67,11 +146,12 @@ export const CheckOut = () => {
 
               <p>billing details</p>
               <Formik
-                initialValues={{ name: "", email: "" }}
+                initialValues={initialState}
                 onSubmit={async (values) => {
                   await new Promise((resolve) => setTimeout(resolve, 500));
                   alert(JSON.stringify(values, null, 2));
                 }}
+                validate={values => validate(values)}
               >
                 <Form>
                   <Grid>
@@ -79,26 +159,41 @@ export const CheckOut = () => {
                       <label>Name</label>
                       <Field
                         placeholder="Alexei Ward"
-                        name="name"
+                        name="field1"
+                        value={state.field1}
+                        onChange={(e: ChangeEvent) =>
+                          updateField("field1", e.target.value)
+                        }
                         type="text"
                       />
+                       <ErrorMessage name="field1" component="p" />
                     </div>
                     <div className="form-group">
                       <label>Email Address</label>
                       <Field
                         placeholder="alexei@mail.com"
-                        name="email"
+                        name="field2"
+                        value={state.field2}
+                        onChange={(e: ChangeEvent) =>
+                          updateField("field2", e.target.value)
+                        }
                         type="email"
                       />
+                         <ErrorMessage name="field2" component="span" />
                     </div>
                   </Grid>
                   <div className="form-group">
                     <label>Phone Number</label>
                     <Field
                       placeholder="+1 202-555-0136"
-                      name="email"
+                      name="field3"
+                      value={state.field3}
+                      onChange={(e: ChangeEvent) =>
+                        updateField("field3", e.target.value)
+                      }
                       type="text"
                     />
+                     <ErrorMessage name="field3" component="p" />
                   </div>
 
                   <p>shipping info</p>
@@ -106,18 +201,39 @@ export const CheckOut = () => {
                     <label>Address</label>
                     <Field
                       placeholder="1137 Williams Avenue"
-                      name="address"
+                      name="field4"
+                      value={state.field4}
+                      onChange={(e: ChangeEvent) =>
+                        updateField("field4", e.target.value)
+                      }
                       type="text"
                     />
+                     <ErrorMessage name="field4" component="p" />
                   </div>
                   <Grid>
                     <div className="form-group">
                       <label>ZIP Code</label>
-                      <Field placeholder="10001" name="email" type="email" />
+                      <Field
+                        placeholder="10001"
+                        value={state.field5}
+                        onChange={(e: ChangeEvent) =>
+                          updateField("field5", e.target.value)
+                        }
+                        name="email"
+                        type="email"
+                      />
                     </div>
                     <div className="form-group">
                       <label>City</label>
-                      <Field placeholder="New York" name="email" type="email" />
+                      <Field
+                        placeholder="New York"
+                        value={state.field6}
+                        onChange={(e: ChangeEvent) =>
+                          updateField("field6", e.target.value)
+                        }
+                        name="email"
+                        type="email"
+                      />
                     </div>
                   </Grid>
 
@@ -127,6 +243,10 @@ export const CheckOut = () => {
                       <Field
                         placeholder="United States"
                         name="country"
+                        value={state.field7}
+                        onChange={(e: ChangeEvent) =>
+                          updateField("field7", e.target.value)
+                        }
                         type="email"
                       />
                     </div>
@@ -172,14 +292,22 @@ export const CheckOut = () => {
                           placeholder="238521993"
                           name="email"
                           type="email"
+                          value={state.field8}
+                          onChange={(e: ChangeEvent) =>
+                            updateField("field8", e.target.value)
+                          }
                         />
                       </div>
                       <div className="form-group">
                         <label>e-Money PIN</label>
                         <Field
                           placeholder="6891"
-                          name="email"
+                          name="pin"
                           type="password"
+                          value={state.field9}
+                          onChange={(e: ChangeEvent) =>
+                            updateField("field9", e.target.value)
+                          }
                         />
                       </div>
                     </Grid>
